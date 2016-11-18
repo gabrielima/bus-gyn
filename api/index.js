6dd781d86185c99e122edeb166c0dae9b7ac7e67
Gabriel de Lima Rabelo <rabeloglr@gmail.com>
2016-11-18 17:33:52 -0200

    Added .editorconfig

--- api/index.js
+++ api/index.js
@@ -3,15 +3,19 @@ var request = require('request');
 
 var app = express()
 
-app.get('/:number', function (req, res) {
-	var data;
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.get('/:number', function (req, res) {
 	request('http://easybus.tk/api/v1/point/' + req.params.number, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
-	    data = body;
+	    console.log(body);
+	    res.send(body);
 	  }
 	})
-  res.send(data)
 })
 
 app.listen(3000, function () {
